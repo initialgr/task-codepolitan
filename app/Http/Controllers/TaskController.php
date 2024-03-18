388d904a6e59e4c5800f0d0389b8ae188d763b71
initialgr <initial.gr.101@gmail.com>
2024-03-18 13:14:34 +0700

    main

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('verified');
+        $this->middleware('is_admin');
+    }
+
+    public function index(Request $request)
+    {
+        if ($request->search) {
+            $tasks = Task::where('task', 'LIKE', "%$request->search%")
+                ->paginate(3);
+
+            return view('task.index', [
+                'data' => $tasks
+            ]);
+        }
+
+        $tasks = Task::paginate(3);
+        return view('task.index', [
+            'data' => $tasks
+        ]);
+    }
+
+    public function create()
+    {
+        return view('task.create');
+    }
+
+
+    public function store(TaskRequest $request)
+    {
+        Task::create([
+            'user' => $request->user,
+            'task' => $request->task,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return $task;
+    }
+
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        return view('task.edit', compact('task'));
+    }
+
+
+    public function update(TaskRequest $request, $id)
+    {
+        $task = Task::find($id);
+
+        $task->update([
+            'user' => $request->user,
+            'task' => $request->task,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/tasks');
+    }
+}
---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('main')
+    <div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
+        <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
+            <span class="fs-5 fw-semibold">Task Lists: {{ $data->total() }} </span>
+            <a href="{{ url('/tasks/create') }}" class="btn btn-sm btn-primary">add</a>
+        </div>
+
+        @foreach ($data as $item)
+            <div class="list-group list-group-flush border-bottom scrollarea">
+                <div class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+                    <div class="d-flex w-100 align-items-center justify-content-between">
+                        <strong class="mb-1">{{ $item->task }}</strong>
+                        <small>Wed</small>
+                    </div>
+                    <div class="col-10 mb-1 small">{{ $item->user }}</div>
+                    <div class="group-action">
+                        <a href="{{ url("/tasks/$item->id/edit") }}" class="badge bg-info text-white">edit</a>
+                        <a href="{{ url("/tasks/$item->id/delete") }}" class="badge bg-danger text-white"
+                            onclick="event.preventDefault();
+                            document.getElementById('{{ $item->id }}').submit();">Delete</a>
+                        <form id="{{ $item->id }}" action="{{ url("/tasks/$item->id") }}" method="POST"
+                            style="display: none;">
+                            @csrf
+                            @method('DELETE')
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        <br>
+        {{ $data->links('pagination::bootstrap-4') }}
+    </div>
+@endsection
